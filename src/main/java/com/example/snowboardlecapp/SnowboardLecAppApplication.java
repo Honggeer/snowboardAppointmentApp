8fed3a61073629e12317703c488d0cddc998f422
Honggeer <honggeer1208@gmail.com>
2024-06-10 19:36:46 -0400

    Created the project: snowboard lecture appointment app

---
+++ src/main/java/com/example/snowboardlecapp/SnowboardLecAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.snowboardlecapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowboardLecAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowboardLecAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/snowboardlecapp/SnowboardLecAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snowboardlecapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowboardLecAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
